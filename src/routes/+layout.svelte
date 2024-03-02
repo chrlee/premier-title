13070101d2088b0e0add3b9980a3eef76958ad62
Christian Lee <christianjslee@gmail.com>
2024-03-01 22:34:48 -0500

    Initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,67 @@
+<script lang="ts">
+	import '../app.postcss';
+	import { AppShell, AppBar, TabGroup, TabAnchor } from '@skeletonlabs/skeleton';
+  import Icon from '@iconify/svelte';
+  import { page } from '$app/stores';
+
+  const year = new Date().getFullYear();
+</script>
+
+<!-- App Shell -->
+<AppShell>
+	<svelte:fragment slot="header">
+		<!-- App Bar -->
+		<AppBar>
+			<svelte:fragment slot="lead">
+        <Icon icon="material-symbols:menu" class="inline md:hidden" style="font-size: 24px;" inline />	
+        <a href="/">
+          <strong class="text-xl uppercase hidden md:block hover:text-primary-400">Premier Title & Settlement Co.</strong>
+        </a>
+			</svelte:fragment>
+      <strong class="text-xl uppercase inline md:hidden">Premier Title & Settlement Co.</strong>
+			<svelte:fragment slot="trail">
+        <TabGroup 
+          justify="justify-center"
+          active="variant-filled-primary"
+          hover="hover:variant-soft-primary"
+          flex="flex-1 lg:flex-none"
+          rounded=""
+          border=""
+          class="bg-surface-100-800-token w-full hidden md:flex"
+        >
+          <TabAnchor href="/">
+            <span>Access Your Closings</span>
+          </TabAnchor>
+          <TabAnchor href="/services" selected={ $page.url.pathname === '/services' }>
+            <span>Services</span>
+          </TabAnchor>
+          <TabAnchor href="/">
+            <span>Specialties</span>
+          </TabAnchor>
+          <TabAnchor href="/">
+            <span>eNotary</span>
+          </TabAnchor>
+          <TabAnchor href="/">
+            <span>About</span>
+          </TabAnchor>
+          <TabAnchor href="/">
+            <span>Contact</span>
+          </TabAnchor>
+        </TabGroup>
+			</svelte:fragment>
+		</AppBar>
+	</svelte:fragment>
+  <!-- Left Sidebar Slot -->
+	<svelte:fragment slot="sidebarLeft">
+		<p>Sidebar Working!</p>
+	</svelte:fragment>
+	<!-- Page Route Content -->
+	<slot />
+  <!-- Page Footer -->
+  <svelte:fragment slot="pageFooter">
+    <div class="text-center m-4">
+      <p> ★ Serving DC, Maryland, and Virginia since 2006. </p>
+      <p> © {year} All rights reserved. </p>
+    </div>
+  </svelte:fragment>
+</AppShell>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,71 @@
+<script>
+	import Icon from "@iconify/svelte";
+
+</script>
+<!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
+
+<div class="container h-full mx-auto flex justify-center items-center">
+	<div class="space-y-10 text-center flex flex-col items-center">
+		<h2 class="h2">Welcome to Skeleton.</h2>
+		<!-- Animated Logo -->
+		<figure>
+			<svg
+				class="fill-token -scale-x-[100%]"
+				xmlns="http://www.w3.org/2000/svg"
+				viewBox="0 0 186 256"
+			>
+      <path d="M92.718,57.02L76.428,71.9V59.37h-14.01v25.41l-24.06,21.91l8.53,9.14l8.3-7.62V160h24.23v-32h27v32h24.25v-52.26l8.43,7.62 l8.32-9.37L92.718,57.02z M133.238,1.616H2v252h181.835V52.708L133.238,1.616z M17.812,17.427h109.002l0.692,0.494v41.012h40.518 v0.198v178.673H17.812V17.427z M147.418,194h-109v-12h109V194z M147.418,216h-109v-12h109V216z"></path>
+		</figure>
+		<!-- / -->
+		<div class="flex relative justify-center space-x-2">
+      <section class="img-bg" />
+			<button
+				class="btn get-qualia-quote bg-primary-500 text-black"
+			>
+				<div class="flex align-items-center">
+          <p>Get an Instant Quote</p>
+          <Icon icon="basil:caret-right-outline" inline style="font-size: 1.5em;"/>
+        </div>
+			</button>
+		</div>
+		<div class="space-y-2">
+			<p>Try editing the following:</p>
+			<p><code class="code">/src/routes/+layout.svelte</code></p>
+			<p><code class="code">/src/routes/+page.svelte</code></p>
+		</div>
+	</div>
+</div>
+
+<style lang="postcss">
+	figure {
+		@apply flex relative flex-col;
+	}
+	figure svg {
+		@apply w-64 h-64 md:w-80 md:h-80;
+	}
+	.img-bg {
+    @apply w-32 h-10 md:w-40 md:h-12;
+		@apply absolute z-[-1] rounded-full blur-[50px] transition-all;
+		animation: pulse 5s cubic-bezier(0, 0, 0, 0.5) infinite,
+			glow 5s linear infinite;
+	}
+	@keyframes glow {
+		0% {
+			@apply bg-primary-400/50;
+		}
+		33% {
+			@apply bg-secondary-400/50;
+		}
+		66% {
+			@apply bg-tertiary-400/50;
+		}
+		100% {
+			@apply bg-primary-400/50;
+		}
+	}
+	@keyframes pulse {
+		50% {
+			transform: scale(1.5);
+		}
+	}
+</style>
