28d7ca61a6cf78050cbb9eadf11e30445294f7ba
Christian Lee <christianjslee@gmail.com>
2024-03-17 08:03:38 -0400

    Services page content

--- src/routes/services/+page.svelte
+++ src/routes/services/+page.svelte
@@ -5,14 +5,84 @@
 <div class="page-container">
   <Accordion>
     <AccordionItem open>
-      <svelte:fragment slot="lead">(icon)</svelte:fragment>
-      <svelte:fragment slot="summary">(summary)</svelte:fragment>
-      <svelte:fragment slot="content">(content)</svelte:fragment>
+      <svelte:fragment slot="summary">Title Search & Examinations</svelte:fragment>
+      <svelte:fragment slot="content">Title Search is a detailed examination of public title records, including deeds, court records, property, and name indexes. Its purpose is to ensure that the seller is the legal owner and has the right to transfer ownership, as well as to discover any defects, liens, or other outstanding claims on the property that could prevent the sale. Making a thorough investigation and review of all title records is crucial to ensure there are no encumbrances on the property.</svelte:fragment>
     </AccordionItem>
     <AccordionItem>
-      <svelte:fragment slot="lead">(icon)</svelte:fragment>
-      <svelte:fragment slot="summary">(summary)</svelte:fragment>
-      <svelte:fragment slot="content">(content)</svelte:fragment>
+      <svelte:fragment slot="summary">Owner's Policies</svelte:fragment>
+      <svelte:fragment slot="content">A homeowner’s policy protects you, the purchaser, against potential losses stemming from faults in your ownership or interest in the property. Safeguarding the equity in your new home necessitates obtaining a title policy.
+
+Standard title insurance coverage addresses risks such as:
+
+Forgery and impersonation
+Lack of competency, capacity, or legal authority of a party
+Deed not signed by a necessary party (co-owner, heir, spouse, corporate officer, or business partner)
+Undisclosed (yet recorded) prior mortgage or lien
+Undisclosed (yet recorded) easement or use restriction
+Erroneous or inadequate legal description
+Lack of a right of access
+Deed not correctly recorded
+An Enhanced Owner’s Policy offers extended coverage to safeguard against additional defects such as:
+
+Off-record matters, including claims for adverse possession or prescriptive easement
+Deed to land with buildings encroaching on land owned by another
+Incorrect survey
+Silent (off-record) liens (e.g., mechanics’ or estate tax liens)
+Pre-existing violations of subdivision laws or zoning ordinances
+The policy provides protection from financial loss due to demands that may be made against the title to your home, up to the cost of the title policy. Additionally, it covers:
+
+Payment of legal costs if the title insurer has to defend your title against a covered claim.
+Payment of successful claims against the title to your home covered by the policy, up to the cost of the policy.</svelte:fragment>
+    </AccordionItem>
+    <AccordionItem>
+      <svelte:fragment slot="summary">Lender's Policies</svelte:fragment>
+      <svelte:fragment slot="content">
+A Lender’s policy safeguards the interests of the mortgage holder. In the event of a title fault causing a loss, the mortgage holder will receive compensation. Commonly known as a loan policy, it is issued to mortgage lenders. This policy covers only the amount of the loan, which typically does not equate to the full property value. Normally, the lender wouldn't be concerned about an adverse claim unless the loan becomes nonperforming, and the claim jeopardizes the lender's ability to foreclose and recover its principal and interest. Additionally, in the event of a claim, there is no provision for covering legal expenses for an uninsured party. When issuing a loan policy, the slight extra cost of an owner’s policy is a worthwhile investment.</svelte:fragment>
+    </AccordionItem>
+    <AccordionItem>
+      <svelte:fragment slot="summary">The American Land Title Association</svelte:fragment>
+      <svelte:fragment slot="content">The American Land Title Association (ALTA) provides endorsement forms used nationwide, sometimes with modifications in different regions. These endorsements offer insurance to lenders, covering losses from various matters including:
+
+The property's title, on which the mortgage is issued, is either not in the borrower's name, subject to defects, liens, or encumbrances, or is unmarketable.
+Lack of right of access to the land.
+Invalidity or unenforceability of the lien created by the mortgage, not being prior to any other existing lien on the property on the policy's date, or being subject to mechanic’s liens under specific circumstances.
+ALTA policy forms also cover the cost of defending insured matters against attacks. However, certain matters are excluded from or excepted in all policies, subject to various conditions.</svelte:fragment>
+    </AccordionItem>
+    <AccordionItem>
+      <svelte:fragment slot="summary">Title Policy Discounts</svelte:fragment>
+      <svelte:fragment slot="content">
+Within ten years of the issuance of the title policy, if the new buyer can obtain the previous title policy issued to the previous owner (seller), we can issue a new title policy at the current reissue rate with a 30% discount.</svelte:fragment>
+    </AccordionItem>
+    <AccordionItem>
+      <svelte:fragment slot="summary">Deeds</svelte:fragment>
+      <svelte:fragment slot="content">Title insurance primarily relies on recorded documents, public records, files, and similar sources. One common document in this process is a deed, which is a written instrument transferring the title or an interest in real property from one party to another. Here are brief descriptions of the most commonly used types of deeds:
+
+General Warranty Deed: This offers the most protection to a real estate buyer. It includes statutory covenants regarding prior conveyance and encumbrance. The seller, or grantor, conveys the property with specific covenants or warranties, legally binding them. These typically include:
+a) Covenant of seisin: The grantor warrants ownership of the property and the legal right to convey it.
+b) Covenant against encumbrances: The grantor warrants that the property is free of any liens or encumbrances unless specifically stated in the deed.
+c) Covenant of quiet enjoyment: The buyer is guaranteed that the title will be good against third parties attempting to establish title to the property.
+d) Covenant of further assurance: The grantor promises to deliver any necessary document or instrument to make the title good.
+
+These warranties in a general warranty deed extend back to the origin of the property, making each grantor in the title chain liable for title problems before and during their ownership.
+
+Special Warranty Deed: This type of deed is less protective of the buyer compared to a general warranty deed. The grantor conveys the property with two warranties: (1) They warrant that they have received title, and (2) they warrant that the property was not encumbered during their ownership period unless stated otherwise. The grantor only warrants the title against their own actions or omissions and nothing prior to their ownership.
+
+Quitclaim Deed: This deed conveys any possible interest of the grantor in the property at the date of the deed without representations of encumbrances on title arising from liens, easements, etc. It is commonly used to release an estate or interest less than a "fee" interest.
+
+Deed of Gift: This type of deed can be any of the above types. It is a transfer of title from one person to another without any consideration or money involved. It is generally exempt from grantor’s tax, but there are certain conditions, so it's advisable to consult a title attorney for detailed information.</svelte:fragment>
+    </AccordionItem>
+    <AccordionItem>
+      <svelte:fragment slot="summary">Good Faith Estimates</svelte:fragment>
+      <svelte:fragment slot="content">
+The new Good Faith Estimate (GFE) form provides clear disclosure of loan terms and closing costs. Our new settlement statement will align with the GFE and disclose any differences from the original figures. We assure you that we can guarantee a maximum 10% variance from the GFE.</svelte:fragment>
+    </AccordionItem>
+    <AccordionItem>
+      <svelte:fragment slot="summary">Referrals</svelte:fragment>
+      <svelte:fragment slot="content">As the real estate market fluctuates, real estate professionals, including title companies, may enter and exit the industry. It is crucial to enlist top-notch real estate professionals for any real estate transaction. The general public may not possess pertinent information on real estate professionals, such as realtors and loan officers, as we do. With experience working with hundreds of realtors, loan officers, and other real estate professionals covering diverse ethnic groups, nationalities, and languages, we can provide unbiased opinions based on various relevant factors to assist you in your selection process. Additionally, we collaborate with surveyors, appraisers, termite companies, home warranties, construction companies, and various other professionals. When you require a referral, please don't hesitate to contact us at 703-658-7007.</svelte:fragment>
+    </AccordionItem>
+    <AccordionItem>
+      <svelte:fragment slot="summary">Other Services</svelte:fragment>
+      <svelte:fragment slot="content">We possess expertise in handling non-traditional settlements, including Short Sales, Bankruptcy, foreclosure, and properties in Probate.</svelte:fragment>
     </AccordionItem>
   </Accordion>
 </div>
